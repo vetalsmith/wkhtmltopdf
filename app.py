fec85f812c5e11d7190e8e0e63fa2adcb544dad4
vetalsmith <31832779+vetalsmith@users.noreply.github.com>
2024-05-27 23:11:29 +1100

    Update app.py

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def application(request):
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
     run_simple(
-        '0.0.0.0', 5000, application, use_debugger=True, use_reloader=True
+        '0.0.0.0', 5000, application, use_debugger=False, use_reloader=True
     )
